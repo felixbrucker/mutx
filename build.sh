5c3e0ae3daaf2374945466ba3b7a1af339bff173
Felix Brucker <felix@felixbrucker.com>
2018-04-13 11:24:10 +0200

    try building arm qt targets

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 ./autogen.sh
-cd depends && make $MAKEJOBS $OPT
-cd $TRAVIS_BUILD_DIR && ./configure --prefix=$TRAVIS_BUILD_DIR/depends/$HOST $CONF
+cd depends && make $MAKEJOBS
+cd $TRAVIS_BUILD_DIR && ./configure --prefix=$TRAVIS_BUILD_DIR/depends/$HOST
 make $MAKEJOBS
 echo "all done"
 exit 0
